c8529f48113e7b30951292506a72d3051a3547f2
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-11-17 21:41:01 -0800

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -27,3 +27,18 @@ int main()
 
 
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=col; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
