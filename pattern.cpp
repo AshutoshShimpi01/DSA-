d9943172b2c323290710281bd581a77af93cc551
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-11-17 21:57:49 -0800

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -42,3 +42,33 @@ int main()
     }
     return 0;
 }
+
+
+*********************************************************************
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;  //holo rectangle pattern
+    cin>>row>>col;
+    
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=col; j++){
+            if(i==1 || i==row || j==1 || j==col){
+                cout<<"#";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+*********************************************************************
