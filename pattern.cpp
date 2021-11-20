e994bbd75378459e7ea92d9156d979b789756a8a
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-11-19 22:26:46 -0800

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -249,3 +249,39 @@ public:
     }
 
 };
+
+*********************************************************************
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+    
+    for(int i=a; i<=a*b; i++)
+    {
+        if(i%a==0 && i%b==0)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+/*
+ output-
+ 
+ Lowest multiple value (LCM)
+ 
+ 5,3
+ out-15
+ */
+*********************************************************************
+
