6da7d920ed23a2a6f7a687f82918241fba0e4259
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-11-21 09:14:42 -0800

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -325,46 +325,5 @@ int main()
  */
 *********************************************************************
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int n,j;
-    cin>>n;
-    
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n-i; j++)
-        {
-            cout<<" ";
-        }
-        for(j=1; j<=n; j++) // for(j=1; j<=i; j++):- 2nd output
-        {
-            cout<<j<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
-
-
-/*
- output-
- 5
-     1 2 3 4 5
-    1 2 3 4 5
-   1 2 3 4 5
-  1 2 3 4 5
- 1 2 3 4 5
- Program ended 
- for(j=1; j<=i; j++):- 2nd output 5
-    1 
-   1 2 
-  1 2 3 
- 1 2 3 4 
-1 2 3 4 5 
- */
-*********************************************************************
 
 
