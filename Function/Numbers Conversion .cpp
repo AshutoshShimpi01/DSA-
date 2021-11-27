9062e840142ca2275c78cc2509b58255f5881462
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-11-26 22:07:09 -0800

    Update Numbers Conversion .cpp

--- Function/Numbers Conversion .cpp
+++ Function/Numbers Conversion .cpp
@@ -1,1 +1,30 @@
-haggsgsjha
+#include <iostream>
+
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+    int ans=0;
+    int x=1;
+    while(n>0)
+    {
+        int y=n%10;
+        ans=ans+(x*y);
+        x=x*2;
+        n=n/10;
+    }
+    return ans;
+
+    }
+int32_t main()
+{
+    int n;
+    cin>>n;
+    cout<<binaryToDecimal(n)<<endl;
+}
+/*
+ 101
+ 5
+ Program ended
+ */
+
