6f4b926d9fa5643a3b3e03b92d8c6bc7c3610043
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-03 22:00:48 +0530

    Update BinarySearch.cpp

--- Arrays.Cpp1/BinarySearch.cpp
+++ Arrays.Cpp1/BinarySearch.cpp
@@ -1,38 +1,37 @@
 #include<iostream>
 using namespace std;
 
-int BinearSearch(int array[],int n,int key)
+int binarySearch(int arr[],int n,int key)
 {
     int s=0;
     int e=n;
-    while(s<=n)
-    {
+    while (s<=e) {
         int mid=(s+e)/2;
-    
-    if(key==array[mid])
-    {
+        if(arr[mid]==key)
+        {
         return mid;
-}
-    else if(array[mid]>key)
+    }
+    else if(arr[mid]>key)
     {
         e=mid-1;
     }
-    else{
+    else
+    {
         s=mid+1;
     }
-    return -1;
+
+return -1;
 }
 
-int main()
-{
-    int n,key;
+    int main(){
+    int n;
     cin>>n;
-    int array[n];
+    int arr[n];
     for(int i=0;i<n;i++)
     {
-        cin>>array[i];
-    }
+        cin>>arr[i];
+}
+    int key;
     cin>>key;
-    cout<<BinarySearch(array,n,key);
-    return 0;
+        cout<<binarySearch(arr, n, key)<<endl;
 }
