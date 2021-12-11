d5f1382d043962e0aef9f0eec780e12cd2a7ace8
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-11 11:47:11 +0530

    Update max_min array.cpp

--- Arrays.Cpp1/max_min array.cpp
+++ Arrays.Cpp1/max_min array.cpp
@@ -27,3 +27,38 @@ int main()
 100 5
 Program ended 
 */
+
+*************************************************
+#include <iostream>
+using namespace std;
+    
+
+int main()
+{
+    int mx=-1999;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        mx=max(mx, arr[i]);
+        cout<<mx<<endl;
+    }
+    return 0;
+    
+}
+/*
+ 6
+ 0 -9 1 3 -4 5
+ 0
+ 0
+ 1
+ 3
+ 3
+ 5
+ Program ended
+ */
