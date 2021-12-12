ae903fc5259378b867cba809a8b1a8caecb0a2d3
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-12 19:45:01 +0530

    Update and rename codechef problems to codechef problems.cpp

--- Common Interview Questions/codechef problems.cpp
+++ Common Interview Questions/codechef problems.cpp
@@ -51,3 +51,47 @@ int main(){
  
  Program ended
  */
+********************************************************
+
+// Record Breaking days Question subarray
+#include <iostream>
+using namespace std;
+    
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n+1];
+    a[n]=-1;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+        if(n==1)
+        {
+            cout<<-1;
+        return 0;
+        }
+    int ans=0;
+    int mx=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>mx && a[i]>a[i+1])
+        {
+            ans ++;
+        }
+        mx=max(mx, a[i]);
+    }
+    cout<<ans<<endl;
+
+    return 0;
+    
+}
+/*
+ 8
+ 1 2 0 7 2 0 2 2
+ 2  // Record Breaking days
+ Program ended
+ */
