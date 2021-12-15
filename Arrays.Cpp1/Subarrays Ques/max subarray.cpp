a6b50b1f852e7fbfb39a03ca5c003251228aa48e
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-15 10:21:24 +0530

    Update max subarray.cpp

--- Arrays.Cpp1/Subarrays Ques/max subarray.cpp
+++ Arrays.Cpp1/Subarrays Ques/max subarray.cpp
@@ -36,3 +36,42 @@ int main()
  4
  Program ended
  */
+
+*******************************************
+#include <iostream> // find maximum subarray sum
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int intMax=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++){
+            int sum=0;
+            for(int k=i;k<=j;k++)
+            {
+                sum+=a[k];
+               
+                //cout<<a[k]<<" ";
+            }
+            intMax=max(intMax,sum);
+            
+        }
+        
+    }
+    
+    cout<<intMax;
+    cout<<endl;
+}
+/*
+ 4
+ -1 4 7 2
+ 13- max sum of array is 13
+ Program ended
+ */
