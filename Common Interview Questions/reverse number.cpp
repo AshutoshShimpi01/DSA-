3867242875216fde2caf46412ba256978f1c40b9
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-15 15:49:24 +0530

    Update and rename reverse number to reverse number.cpp

--- Common Interview Questions/reverse number.cpp
+++ Common Interview Questions/reverse number.cpp
@@ -11,8 +11,8 @@ int main()
     cin>>i;
     while(i>0)
     {
-        rev=(rev*10)+i%10;
-        i=i/10;
+        rev=(rev*10)+i%10; // i=234 then 234/10= always reminder=lastdigit=4
+        i=i/10; // 234/10=quotient is 23.4 so int is 23 and 4 is ignor. Then 23/10=2.3 so 3 ignore
         
     }
     cout<<rev;
