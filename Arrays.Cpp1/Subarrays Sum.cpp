cf34d1a95b465c5657ca2bf037633090ce51577f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2021-12-15 10:00:44 +0530

    Update Subarrays Sum.cpp

--- Arrays.Cpp1/Subarrays Sum.cpp
+++ Arrays.Cpp1/Subarrays Sum.cpp
@@ -101,3 +101,42 @@ int main()
  4 -inside 4th index
  Program ended
  */
+*****************************************
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++){
+            for(int k=i;k<=j;k++)
+            {
+                cout<<a[k]<<" ";
+            }
+            cout<<endl;
+        }
+       
+    }
+}
+/*
+ 4
+ -1 2 7 4
+ -1
+ -1 2
+ -1 2 7
+ -1 2 7 4
+ 2
+ 2 7
+ 2 7 4
+ 7
+ 7 4
+ 4
+ Program ended
+ */
