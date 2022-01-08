40e1904b9686ae7ab0f3767fb1bd3de22b7d9f5f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-01-08 11:23:03 +0530

    Update 1st program.c

--- Pointer.c/1st program.c
+++ Pointer.c/1st program.c
@@ -15,3 +15,36 @@ int main() {
  size of pointer 8
  Program ended with exit code: 0
  */
+
+*********************************************
+
+
+#include <stdio.h>
+int main()
+{
+    
+    int arr[5]={10,20,30,40,50};
+    int *p=arr;
+    char array[5]={'ashu','rushi','pawan','nayan','abhi'};
+    char *pf=array;
+  
+    printf("%d\n",p);
+    printf("%d\n",arr);
+    printf("%u\n",arr+1);
+    printf("%c\n",array);
+    printf("%c\n",pf);
+    printf("%c\n",array+1);
+    printf("Print the array:\n",array+1);
+    return 0;
+}
+/*
+ Output-
+ 1876947456
+ 1876947456
+ 1876947460
+ \350
+ \350
+ \351
+ Print the array:
+ Program ended with exit code: 0
+ */
