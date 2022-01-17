def397cf12b06813ed602fda11e034ee3c00f9ed
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-01-17 19:16:15 +0530

    Update prime.cpp

--- Function/prime.cpp
+++ Function/prime.cpp
@@ -43,3 +43,26 @@ int main()
  Program ended
  */
 
+************************************
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int b;
+    cin>>b;
+    int i;
+    for(i=2; i<b;i++)
+    {
+        if(b%i==0)
+        {
+            cout<<"Non-Prime"<<endl;
+            break;
+        }
+    }
+        if(i==b)
+        {
+            cout<<"Prime"<<endl;
+        }
+    return 0;
+}
