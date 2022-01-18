ec59f36333107312a8979da93ad9c9ac4c24b85b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-01-18 11:51:42 +0530

    Update Max val.c

--- Function/Max val.c
+++ Function/Max val.c
@@ -26,3 +26,62 @@ int main()
  
     return 0;
 }
+
+
+********************************
+
+
+
+//
+//  main.c
+//  arrays.c
+//
+//  Created by ♠️ᴅᴀᴅ's ɢɪғᴛ♠️ on 03/01/22.
+//
+
+#include<stdio.h>
+
+int Maximum(int no1,int no2,int no3)
+{
+   if(no1 > no2 && no1 > no3)
+   {
+       return no1;
+   }
+   else if(no2 > no1 && no2 >no3)
+   {
+       return no2;
+   }
+    else
+   {
+       return no3;
+   }
+}
+int main()
+{
+    int a=0;
+    int b=0;
+    int d=0;
+    
+    printf("Enter The 1st No.\n\t");
+    scanf("%d",&a);
+    printf("Enter The 2nd No.\n\t");
+    scanf("%d",&b);
+    printf("Enter The 3rd No.\n\t");
+    scanf("%d",&d);
+    
+    int c = Maximum(a,b,d);
+    printf("%d is Greater Number\n",c);
+    return 0;
+}
+
+// output
+/*
+ Enter The 1st No.
+     30
+ Enter The 2nd No.
+     10
+ Enter The 3rd No.
+     5
+ 30 is Greater Number
+ Program ended
+ */
