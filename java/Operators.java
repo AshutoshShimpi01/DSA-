865ce1145eb8df0533c2d23b4e00b05b748554ed
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-01-29 21:52:11 +0530

    Update Operators.java

--- java/Operators.java
+++ java/Operators.java
@@ -9,3 +9,31 @@ public class ja
 		System.out.println(m);
 	}
 }
+
+// SWITCH.JAVA
+
+
+public class ja
+{
+ public static void main(String[] args) 
+	{
+		int n=9;
+		switch(n)
+		{
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+		    System.out.println("Two");	
+		    break;
+		case 3:
+		    System.out.println("Three");
+		    break;
+		case 4:
+		    System.out.println("Four");	
+		    break;
+		default:
+		    System.out.println("No match");		
+		}
+	}
+}
