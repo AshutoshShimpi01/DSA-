270c8731b7d954d29c7c43bc921a6f3ce153b56f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-01-30 09:19:12 +0530

    Update basic.java

--- java/basic.java
+++ java/basic.java
@@ -1,1 +1,27 @@
 
+public class ja
+{
+ public static void main(String[] args) 
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+    }
+	
+	
+}
+/*
+OUTPUT-
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+*/
