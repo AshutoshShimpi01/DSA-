b8d59611666de50ab225deb2ee75ab041c8ac54e
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-02-01 21:43:37 +0530

    Update 2D array.java

--- java/Arrays.java/2D array.java
+++ java/Arrays.java/2D array.java
@@ -57,3 +57,24 @@ public class ja
  2 4 6 8
  5 6 7 8 5
 */
+************************************************
+
+// 1D ARRAY
+
+public class ja
+{
+	public static void main(String[] args) {
+		int a[]={1,25,54,60,59,88};
+
+		for(int i=0;i<6;i++)
+		{
+			System.out.print(" "+a[i]);
+		}
+		System.out.println();
+	}
+}
+/*
+OUTPUT=
+
+1 25 54 60 59 88
+*/
