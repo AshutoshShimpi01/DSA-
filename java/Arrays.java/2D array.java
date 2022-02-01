85e2a31909852faf3df34027252e8dd56f08b07f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-02-01 13:14:51 +0530

    Create 2D array.java

---
+++ java/Arrays.java/2D array.java
@@ -0,0 +1,25 @@
+public class ja
+{
+	public static void main(String[] args) {
+		int a[][]={
+			{1,2,3},
+			{2,4,6,8},
+			{5,6,7,8,5}
+		};
+
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a[i].length;j++) // if arrays size is not fixed so use .length
+			{
+				System.out.print(" "+ a[i][j]);
+			}
+			System.out.println();
+		}
+		}
+	}
+/*
+1 2 3
+ 2 4 6 8
+ 5 6 7 8 5
+
+*/
