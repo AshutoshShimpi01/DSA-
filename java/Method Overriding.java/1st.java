54b685b3ce634ed52a84a1c2bcdc566bd1294d23
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-02-02 11:55:36 +0530

    Update 1st.java

--- java/Method Overriding.java/1st.java
+++ java/Method Overriding.java/1st.java
@@ -38,3 +38,34 @@ Brother is Keshav
 
 
 */
+
+    //         ******CASE-2*******
+
+
+class Ashu
+{
+	public void shri()
+	{
+		System.out.println("Brother is Keshav");
+	}
+}
+class Yogesh extends Ashu
+{
+	public void shri()
+	{
+		System.out.println("Is a Big Brother of Ashu,shri and keshav");
+	}
+}
+
+public class java
+{
+	public static void main(String[] args) {
+		
+		Yogesh obj1 = new Yogesh();
+
+        obj1.shri();
+	}
+}
+
+//   OUTPUT=
+//   Is a Big Brother of Ashu,shri and keshav
