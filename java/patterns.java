f0deca2fae37e4bb50e60c5b582c0f95b7f4204a
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-02-02 11:22:10 +0530

    Create patterns.java

---
+++ java/patterns.java
@@ -0,0 +1,27 @@
+
+public class ja
+{
+ public static void main(String[] args) 
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++) // inner loop execute multiple times
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+    }
+	
+	
+}
+/*
+OUTPUT-
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+*/
