91ef24ff2aeb4ffd42f41079ac08958b3c4a41e0
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-02-06 11:47:02 +0530

    Update sum.java

--- java/Arrays.java/sum.java
+++ java/Arrays.java/sum.java
@@ -16,3 +16,41 @@ public class java
 		System.out.println(result);
 	}
 }
+
+
+// Multiply.java
+
+
+
+
+public class java {
+
+//User defined Method
+public int multiply(int a, int b, int c){
+int result = a * b * c;
+return result;
+}
+
+public static void main (String [] args){
+
+//Create Object
+java abc = new java();
+
+//Call Method
+int x = abc.multiply(11, 25, 35);
+System.out.println(x);
+
+System.out.println(abc.multiply(10, 25, 35));
+System.out.println(abc.multiply(2,2,2));
+}
+}
+
+/*
+
+9625
+8750
+8
+
+
+
+*/
