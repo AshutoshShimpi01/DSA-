6db9227d2df5842b70053b516486d84084369d08
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-03 19:21:39 +0530

    Update Inheritance.java

--- java/Inheritance.java
+++ java/Inheritance.java
@@ -52,7 +52,7 @@ OUTPUT=
 *********************************************************
 
 // This is a Multi Level Inheritance 
-
+// 1st Example
 class Calculator
 {
 	public int add(int i,int j) 
@@ -100,4 +100,55 @@ OUTPUT=
 20
 4
 15
+*/
+
+
+// 2nd Example :-
+
+class A
+{
+ void showA()
+  {
+    System.out.println("a class method");
+  }
+}
+class B extends A
+{
+
+void showB()
+{
+  System.out.println("b class method");
+}
+}
+class C extends B
+{
+
+void showC()
+{
+  System.out.println("c class method");
+}
+
+public static void main(String[] args) {
+  A ob1=new A();
+  ob1.showA();
+
+  B ob2=new B();
+  ob2.showA();
+  ob2.showB();
+
+  C ob3=new C();
+  ob2.showA();
+  ob2.showB();
+  ob3.showC();
+}
+
+}
+
+/*
+a class method
+a class method
+b class method
+a class method
+b class method
+c class method
 */
