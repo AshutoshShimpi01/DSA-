b9fcfd2659b48fc178d2ffa8616f66a6d0e714a7
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-04 18:20:38 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -366,5 +366,72 @@ int main()
 1 2 3 4 5 
  */
 *********************************************************************
+// BUTTERFLY PATTERN
+
+import java.util.Scanner;
 
 
+class java
+{
+  public static void main(String[] args) 
+  {
+    int n=5;
+
+    for(int i=1;i<=n;i++)
+    {
+
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+
+      int spaces=2 * (n-i);
+      for(int j=1;j<=spaces;j++)
+      {
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+
+    for(int i=n;i>=1;i--)
+    {
+
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+
+      int spaces=2 * (n-i);
+      for(int j=1;j<=spaces;j++)
+      {
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    }
+ 
+  }
+  
+  
+/*
+*        *
+**      **
+***    ***
+****  ****
+**********
+**********
+****  ****
+***    ***
+**      **
+*        *
+(base) ashutoshsanjayshimpi@Ashutoshs
+*/
