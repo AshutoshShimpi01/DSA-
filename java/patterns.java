c611852bdb9b221f9b23bf7b4a887a4b319ccb27
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-04 17:35:09 +0530

    Update patterns.java

--- java/patterns.java
+++ java/patterns.java
@@ -209,3 +209,39 @@ class java
 12
 1
 */
+-------------------------------------------------
+import java.util.Scanner;
+
+
+class java
+{
+  public static void main(String[] args) 
+  {
+    
+    int n=5;
+    int number=1;
+    for(int i=1;i<=n;i++) // OUTER LOOP
+    {
+      for(int j=1;j<=i;j++) //1st INNER LOOS
+      {
+        System.out.print(number+ " ");
+        number++;
+        
+      }
+      System.out.println();
+
+    }
+      
+
+    }
+ 
+  }
+  
+  
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+*/
