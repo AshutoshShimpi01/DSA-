a1625b993b87be64213d36b710940c9c64c33438
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-04 16:27:39 +0530

    Update patterns.java

--- java/patterns.java
+++ java/patterns.java
@@ -74,3 +74,45 @@ class java
 
 
 */	
+
+------------------------------------------
+	
+import java.util.Scanner;
+
+
+class java
+{
+  public static void main(String[] args) 
+  {
+    
+    
+    for(int i=1;i<=4;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+        
+      }
+         System.out.println();
+
+    }
+ 
+  }
+  
+}
+ 
+   
+
+
+    
+  
+/*
+
+*
+**
+***
+****
+
+
+
+*/	
