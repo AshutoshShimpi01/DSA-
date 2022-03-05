617a244058faed8739c78c8d25c6653856b257e7
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-05 20:06:15 +0530

    Update patterns.java

--- java/patterns.java
+++ java/patterns.java
@@ -398,6 +398,8 @@ class java
 5 5 5 5 5 
 */
 --------------------------------------------------
+
+//Palindrome Serial
 import java.util.Scanner;
 
 
@@ -448,3 +450,60 @@ class java
 
 */
 ---------------------------------------------
+//DIAMOND
+import java.util.Scanner;
+
+
+class java
+{
+  public static void main(String[] args) 
+  {
+    int n=5;
+
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n-i;j++)
+      {
+        System.out.print(" ");
+      }
+      
+      for(int j=1;j<=2*i-1;j++)
+      {
+        System.out.print("*");
+      }
+     
+      System.out.println();
+    }
+
+    for(int i=n;i>=1;i--)
+    {
+      for(int j=1;j<=n-i;j++)
+      {
+        System.out.print(" ");
+      }
+      
+      for(int j=1;j<=2*i-1;j++)
+      {
+        System.out.print("*");
+      }
+     
+      System.out.println();
+    }
+}
+ 
+  }
+  
+  
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
