c62fb68fc4987071693bf7fff7e5ca8e91855094
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 19:22:14 +0530

    Update StringBulider.java

--- java/Basic programs.java/StringBulider.java
+++ java/Basic programs.java/StringBulider.java
@@ -29,3 +29,33 @@ if we want Tonny so we write:- sb.insert(2,'n');
  OR
 if we want to delete the extra n,so write:- sb.delete(2,3);  so delete only 2nd index not 3rd.
 */
+---------------------------------
+ 
+ import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+
+
+  StringBuilder sb=new StringBuilder("H");
+
+  sb.append("e");
+  sb.append("l");
+  sb.append("l");
+  sb.append("o");
+
+  System.out.println(sb);
+  
+   }
+}
+ 
+  
+/*
+Hello
+*/
+ 
+ 
