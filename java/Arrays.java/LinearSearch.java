aa8eff2cc58573bb2d3adca1d4e6e8d52a05a581
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 08:34:10 +0530

    Create LinearSearch.java

---
+++ java/Arrays.java/LinearSearch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+   Scanner sc=new Scanner(System.in);
+   int size=sc.nextInt();
+
+   int n[]=new int[size];
+
+   for(int i=0;i<size;i++)
+   {
+   n[i]=sc.nextInt();
+   }
+   int x=sc.nextInt();
+
+   for(int i=0;i<n.length;i++)
+   {
+    if(n[i]==x)
+
+    System.out.println(i);
+   }
+}
+ 
+  }  
+/*
+5
+23
+12
+32
+35
+66
+X:
+35
+index of x:
+3
+*/
