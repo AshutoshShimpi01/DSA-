e77f4e2c4316304481e6d4373a344654af2b01f0
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 08:28:14 +0530

    Update arrayscanner.java

--- java/Arrays.java/arrayscanner.java
+++ java/Arrays.java/arrayscanner.java
@@ -36,3 +36,47 @@ public class java
 35
 66
 */
+
+-----------------------------
+import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+   Scanner sc=new Scanner(System.in);
+   int size=sc.nextInt();
+
+   int n[]=new int[size];
+
+   for(int i=0;i<size;i++)
+   {
+   n[i]=sc.nextInt();
+   }
+   int x=sc.nextInt();
+
+   for(int i=0;i<n.length;i++)
+   {
+    if(n[i]==x)
+
+    System.out.println(i);
+   }
+}
+ 
+  }  
+/*
+5
+23
+12
+32
+35
+66
+
+23
+12
+32
+35
+66
+*/
