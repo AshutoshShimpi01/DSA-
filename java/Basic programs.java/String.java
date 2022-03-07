ddc127cc26b74ebaa5f5ee146d1f3bbf03220b18
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 18:16:03 +0530

    Update String.java

--- java/Basic programs.java/String.java
+++ java/Basic programs.java/String.java
@@ -53,3 +53,46 @@ System.out.println(fullname.length);
 OUTPUT is: 10
 (space also included)
 */
+
+------------------------------------------------
+ 
+ import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+  
+  String firstname= "Tony";
+  String lastname="Stark";
+  String fullname=(firstname+lastname);
+
+   System.out.println(fullname);
+
+
+   for(int i=0;i<fullname.length();i++)
+   {
+    System.out.println(fullname.charAt(i));
+   }
+
+   
+    
+   }
+}
+ 
+  
+/*
+TonyStark
+T
+o
+n
+y
+S
+t
+a
+r
+k
+
+*/
