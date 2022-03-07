0283ac7d21cc3f1c5f702abfb50a3f0e9de7e7bc
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 18:04:46 +0530

    Create String.java

---
+++ java/Basic programs.java/String.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+   Scanner sc=new Scanner(System.in);
+   
+   String name=sc.nextLine();
+
+   System.out.println("My name is: "+name);
+    
+   }
+}
+ 
+  
+/*
+Ashutosh Sanjay Shimpi
+My name is: Ashutosh Sanjay Shimpi
+
+if we only take String name=sc.next(); then only print Ashutosh not all
+*/
