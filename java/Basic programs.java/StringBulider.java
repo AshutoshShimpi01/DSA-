32a0f1b92e7122c4fdaa9531d87708ecd8b44862
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-07 19:13:40 +0530

    Create StringBulider.java

---
+++ java/Basic programs.java/StringBulider.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+
+
+  StringBuilder sb=new StringBuilder("Tony");
+  System.out.println(sb);
+
+  sb.insert(0,'S');
+  System.out.println(sb);
+
+  
+   }
+}
+ 
+  
+/*
+Tony
+STony
+*/
