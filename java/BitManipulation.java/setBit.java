36cfa47f017b687afddd95a951a1c68fad29d560
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-09 19:57:48 +0530

    Update setBit.java

--- java/BitManipulation.java/setBit.java
+++ java/BitManipulation.java/setBit.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+
+    int n = 5;
+
+    int pos = 1;
+
+    int bitMask = 1<<pos;
+
+    int newNum = bitMask | n;
+    System.out.println(newNum);
+    
+
+  
+   }
+}
+ 
+  
+/*
+
+7
+
+*/
