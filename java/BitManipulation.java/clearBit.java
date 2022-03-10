475774694be1946a6da31c7780d7ec2a587fb19d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-10 09:06:52 +0530

    Create clearBit.java

---
+++ java/BitManipulation.java/clearBit.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class java
+{
+ 
+  public static void main(String[] args) 
+  {
+
+    int n = 5;
+
+    int pos = 2;
+
+    int bitMask = 1<<pos;
+    int notBitMask = ~(bitMask);
+
+    int newNum = notBitMask & n;
+    System.out.println(newNum);
+    
+
+  
+   }
+}
+ 
+  
+/*
+
+1
+------------------
+
+n=5(101)
+then bitMask=1<<pos(1)
+we get= 0001=0100
+we got, bitMask=0100
+
+now ~(not)=inverseallbits:- 1011
+
+now apply & with n
+
+1011
+0101
+-----
+0001(1)
+
+*/
