b2e9ad37cc9a6794c7a1750fcd8f3615e05daceb
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-12 13:25:55 +0530

    Update prime.cpp

--- Function/prime.cpp
+++ Function/prime.cpp
@@ -39,6 +39,76 @@ int main() {
 
 
 
+#include <iostream>
+using namespace std;
+
+int is_Numberprime(int num)
+{
+    int reminder=0;
+    for(int i=2;i<=num/2;i++)
+    {
+        reminder=num%i;
+        
+        if(reminder==0)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+}
+int main() {
+    int isPrime=0;
+    for(int i=1;i<=100;i++)
+    {
+        isPrime=is_Numberprime(i);
+
+        if(isPrime==0)
+            {
+                cout<<i<<endl;
+            }
+    }
+    
+    
+    
+    
+    
+
+    return 0;
+
+}
+
+/*
+ 1
+ 2
+ 3
+ 5
+ 7
+ 11
+ 13
+ 17
+ 19
+ 23
+ 29
+ 31
+ 37
+ 41
+ 43
+ 47
+ 53
+ 59
+ 61
+ 67
+ 71
+ 73
+ 79
+ 83
+ 89
+ 97
+ */
+
+    
+    
 
 
 
