1f7fa905cddb104b3a235a99bbc70a15716d9172
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-13 11:57:06 +0530

    Update Palindrome numb.cpp

--- Common Interview Questions/Palindrome numb.cpp
+++ Common Interview Questions/Palindrome numb.cpp
@@ -1,3 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int temp=n;
+    int rev=0,rem;
+    
+    while(temp!=0)
+    {
+        rem=temp%10;
+        rev=rev*10+rem;
+        temp=temp/10;
+        
+        cout<<"reminder is:"<<rem<<"  ";
+        cout<<"reverse is:"<<rev<<" ";
+        cout<<"temp is:"<<endl;
+    }
+    if(n==rev)
+    {
+    cout<<n<<"palendrome no";
+    }
+    else
+    {
+        cout<<n<<"not palindrome";
+    }
+    return 0;
+}
+/*
+ 
+ 1221
+ reminder is:1  reverse is:1 temp is:
+ reminder is:2  reverse is:12 temp is:
+ reminder is:2  reverse is:122 temp is:
+ reminder is:1  reverse is:1221 temp is:
+ 1221palendrome no
+ 
+ */
+
+    
+    
+
+
+
+
+
+
+
+
+
+
 #include<iostream> 
 using namespace std;
 
