d70121f2ef0a70eaa2863f029351bb1c36a428c1
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-22 19:23:40 +0530

    Update reverse number.cpp

--- Common Interview Questions/reverse number.cpp
+++ Common Interview Questions/reverse number.cpp
@@ -26,3 +26,53 @@ return 0;
  654321
  Program ended
  */
+**************____________************
+
+//USING RECURSION
+
+#include <iostream>
+using namespace std;
+
+void inc(int n)
+{
+    if(n==1)
+    {
+        cout<<"1"<<endl;
+        return;
+    }
+    inc(n-1);
+    cout<<n<<endl;
+}
+void dec(int n)
+{
+    if(n==1)
+    {
+        cout<<"1"<<endl;
+        return;
+    }
+    cout<<n<<endl;
+    dec(n-1);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    inc(n);
+    dec(n);
+    return 0;
+}
+/*
+ 5
+ 1
+ 2
+ 3
+ 4
+ 5
+ 5
+ 4
+ 3
+ 2
+ 1
+ */
+
