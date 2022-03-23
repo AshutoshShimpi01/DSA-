3dfa4ee03eabc9d3344cc43c48b45aa2bc51f66b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-23 19:11:40 +0530

    Update firstOcc.cpp

--- Recursion.cpp/firstOcc.cpp
+++ Recursion.cpp/firstOcc.cpp
@@ -1,30 +1,47 @@
-// find index
-
 #include <iostream>
 using namespace std;
 
-int checjindex(int a[],int i,int n,int key )
+int firstOcc(int a[],int n,int i,int key)
 {
-    if(a[i]==n)
+    if(i==n)
     {
         return -1;
     }
-    if(a[i]==key)
+    if(a[i] == key)
     {
         return i;
     }
-    return checjindex(a, n, i+1, key);
+   return firstOcc(a,n, i+1,key);
 }
 
+int lastOcc(int a[],int n,int i,int key)
+{
+    if(i==n)
+    {
+        return -1;
+    }
+    int restArray = lastOcc(a,n,i+1,key);
+    if(restArray != -1)
+    {
+        return restArray;
+    }
+    if(a[i] == key)
+    {
+        return i;
+    }
+    return -1;
+}
 int main()
 {
     
     int a[] = {1,2,3,4,2,5};
     
-    cout<<checjindex(a,6,0,2);
+    cout<<"First Occerance of index 2 is:"<<firstOcc(a, 6, 0, 2)<<endl;
+    cout<<"Last Occerance of index 2 is:"<<lastOcc(a,6,0,2)<<endl;
 
     return 0;
 }
 /*
- 1
+ First Occerance of index 2 is:1
+ Last Occerance of index 2 is:4
  */
