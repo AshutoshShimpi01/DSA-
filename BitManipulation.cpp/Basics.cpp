9528d831243ea00c0f7700ab1c751409e2fe6e1d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-26 20:19:37 +0530

    Create Basics.cpp

---
+++ BitManipulation.cpp/Basics.cpp
@@ -0,0 +1,31 @@
+/*
+to get same no.
+n=546
+5*100+4*10+6*1=546
+
+to get lastdigit
+n=123
+n%10=3
+
+to get starting no.
+n=123
+n/10=12
+
+to get reverse no.
+n=123, ans=0
+formula- digit= n%10
+ans=ans*10+digit= 0*10+3=3,3*10+2=32,32*10+1=321
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
