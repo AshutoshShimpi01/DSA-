94e487a10689230f7ac49c2fb4c9e62c0a97f5ec
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-27 17:34:51 +0530

    Update PowerOf(2).cpp

--- LeetCode.cpp/PowerOf(2).cpp
+++ LeetCode.cpp/PowerOf(2).cpp
@@ -42,3 +42,59 @@ int main()
  
  cube of 2=8 2*2*2=8
  */
+
+_____________________________
+    
+// WITHOUT BROUTFORCE-
+    
+ //APROCH 2
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool isPower(int n)
+{
+    int ans=1;
+    
+    for(int i=0;i<=30;i++)
+    {
+        if(ans==n )
+        {
+            return 1;
+        }
+        
+        if(ans < INT_MAX/2)
+        {
+            ans = ans * 2;
+            
+        }
+        
+    }
+    return 0;
+}
+    
+int main()
+{
+    int n;
+    cin>>n;
+    int check = isPower(n);
+    
+    if(check == 1)
+    {
+        cout<<"true";
+    }
+   if(check==0)
+    {
+        cout<<"false";
+    }
+    return 0;
+}
+    
+
+/*
+ 64
+ true
+ 
+ 7
+ false
+ */
