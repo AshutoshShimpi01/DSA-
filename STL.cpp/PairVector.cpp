ff6831c63797a036bf5108e73e869a3f3c3852b8
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-29 16:38:03 +0530

    Create PairVector.cpp

---
+++ STL.cpp/PairVector.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void printVec(vector<pair<int,int> > &v)
+{
+    cout<<"size: " << v.size() << endl;
+    for(int i=0;i<v.size();++i)
+    {
+            cout<<v[i].first<< " "<<v[i].second<<endl;
+    }
+        cout<<endl;
+}
+int main()
+{
+    vector<pair<int,int> > v = {{1,2},{2,3},{3,4}};
+    printVec(v);
+}
+
+/*
+ size: 3
+ 1 2
+ 2 3
+ 3 4
+
+ */
