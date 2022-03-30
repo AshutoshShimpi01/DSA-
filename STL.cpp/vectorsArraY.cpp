5bf7b7b5d460c1cca770747a133c73b1ab804a1b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-03-30 08:59:39 +0530

    Update vectorsArraY.cpp

--- STL.cpp/vectorsArraY.cpp
+++ STL.cpp/vectorsArraY.cpp
@@ -42,3 +42,70 @@ int main()
  size: 2
  1 2 
  */
+
+__________________________________
+    
+ 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void printVec(vector<int> &v)
+{
+    cout<<"size: "<<v.size()<<endl;
+    for(int i=0;i<v.size();++i)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int N;
+    cin>>N;
+    vector<vector<int>> v;
+    for(int i=0;i<N;++i)
+    {
+        int n;
+        cin>>n;
+        vector<int> temp;
+    
+    for(int j=0;j<n;++j)
+    {
+        int x;
+        cin>>x;
+        temp.push_back(x);
+    }
+    v.push_back(temp);
+    }
+v[0].push_back(10);  // at 0th vector add 10
+v.push_back(vector<int> ());
+for(int i=0;i<v.size();++i)
+{
+    printVec(v[i]);
+}
+cout<<v[0][1];
+}
+
+/*
+ 3
+ 3
+ 1 2 3
+ 3
+ 3 4 5
+ 2
+ 1 2
+ 
+ Output-
+ size: 4
+ 1 2 3 10
+ size: 3
+ 3 4 5
+ size: 2
+ 1 2
+ size: 0
+
+ 2
+ */
+
