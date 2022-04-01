240dbd6f80c0c2de50e6a0c74babd71f0b17d333
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-04-01 13:00:50 +0530

    Update BasicsFunc.cpp

--- Function/BasicsFunc.cpp
+++ Function/BasicsFunc.cpp
@@ -3,4 +3,24 @@
 Refrences(&)
 Arrays always pass by Refrences not pass by values
 
+
+REFRENCES-
+
+void incerment(int n)   //Pass by value         // void increment(int &n)= (Pass by reffrence) Now accual variable passs 
+{
+    n++;                                                     outpUt- 3
+                                                                     4  
+}. 
+int main()
+{
+    int a=3;
+    cout<<a<<endl;
+     incerment(a);
+    cout<<a<<endl;
+}
+/*
+ 3
+ 3   // not increase value coz variable copy pass to func not variable. by passing &n so
+ */
+
 */
