362e296d0b35c5bfa4a1d7c7ac24dd40496d3ce9
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-04-01 12:27:37 +0530

    Create Sum.cpp

---
+++ Function/Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int sum(int a,int b)
+{
+    int result = a+b;
+    return result;
+}
+int main()
+{
+   cout<< sum(2,3);
+}
+/*
+ 5
+ */
