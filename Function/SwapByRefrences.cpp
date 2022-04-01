d68f5663613896b4520a5cacabf747abbd264913
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-04-01 12:56:04 +0530

    Create SwapByRefrences.cpp

---
+++ Function/SwapByRefrences.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void swap(int &a,int &b) //Passing by refrences(&)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+   
+}
+int main()
+{
+    int a=3;
+    int b=5;
+    cout<<a<<" "<<b<<endl;
+    swap(a,b);
+    cout<<a<<" "<<b<<endl;
+}
+/*
+ 3 5
+ 5 3
+ */
