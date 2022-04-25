e4f124370584a55213ac67e75e81b34d2d17bf86
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-04-25 23:06:55 +0530

    Update Basic.js

--- Web Devlopment/HTML/JavaScript/Basic.js
+++ Web Devlopment/HTML/JavaScript/Basic.js
@@ -29,6 +29,20 @@ var name = prompt("Composed your name");
 var firstName = name.slice(0,1);
 var upperF = firstName.toUpperCase();
 var newName = name.slice(1,name.length);
+newName = newName.toLowerCase();
 var fname = upperF+newName;
 alert(fname);
+*/
+
+/* Life in Weeks Code
+function lifeInweeks(age)
+{
+  var yearsRemaining = 90 - age;
+  var days = yearsRemaining * 365;
+  var weeks = yearsRemaining * 52;
+  var month = yearsRemaining * 12;
+
+  console.log("you have "+ days + "days, "+ "weeks, and "+ months + "months left.");
+}
+lifeInweeks(12);
 */
