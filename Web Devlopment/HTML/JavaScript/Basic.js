d3f66fe66113f9f832e5da5e290bb631a860354e
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-04-25 19:34:45 +0530

    Create Basic.js

---
+++ Web Devlopment/HTML/JavaScript/Basic.js
@@ -0,0 +1,34 @@
+/*
+Tweeter alert Box-
+var tweet = prompt("Compose Your Tweet: ");
+var tweetCount = tweet.length;
+alert("you have written" + tweetCount + "Chracter you have " + (140 - tweetCount) + "chracter remaining." );
+
+*/
+
+
+/*
+varName.Slice():-
+var tweet = prompt("compose your tweet");
+var tweetUnder140 = tweet.slice(0,120);
+alert(tweetUnder140);
+    OR
+alert(prompt("composed your Tweet").slice(0,140));
+*/
+
+
+/*
+var name = "Ashutosh";
+name = name.toUpperCase();
+name = name.LowerCase();
+*/
+
+/* Firstlet toUpperCase
+name=ashutosh To name=Ashutosh
+var name = prompt("Composed your name");
+var firstName = name.slice(0,1);
+var upperF = firstName.toUpperCase();
+var newName = name.slice(1,name.length);
+var fname = upperF+newName;
+alert(fname);
+*/
