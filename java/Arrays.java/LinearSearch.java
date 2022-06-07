0b1971a502421eb83365675bf51962a9fd18bb68
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-06-07 11:32:04 +0530

    Update LinearSearch.java

--- java/Arrays.java/LinearSearch.java
+++ java/Arrays.java/LinearSearch.java
@@ -38,3 +38,40 @@ X:
 index of x:
 3
 */
+
+
+----------------------------------
+ 
+ import java.lang.*;
+import java.util.*;
+
+class java
+{
+public static void main(String args[])
+{
+
+
+int arr[] = {4,10,2};
+int key =6;
+for(int i=0;i<arr.length;i++)
+{
+
+if(i==key)
+{
+
+System.out.println(i);
+System.exit(0);
+
+}
+
+}
+
+System.out.println("Not Found");
+
+
+}
+
+}
+
+
+//output-Not Found
