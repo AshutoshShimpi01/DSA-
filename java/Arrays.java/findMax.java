9203ce8e0684abe12d4248142fe0cf9d465958ac
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-06-07 12:02:46 +0530

    Update findMax.java

--- java/Arrays.java/findMax.java
+++ java/Arrays.java/findMax.java
@@ -35,4 +35,53 @@ System.out.println(max);
 
 // output- 10
 
+------------------------------------
+  
+//SECOND LAGREST ELEMENT
+  
+  
+import java.lang.*;
+import java.util.*;
+
+class java
+{
+public static void main(String args[])
+{
+
+Scanner sc = new Scanner(System.in);
+
+
+int arr[] = {4,10,2,6,15,12};
+
+int max1=arr[0];
+int max2 = arr[0];
+
+for(int i=0;i<arr.length;i++)
+{
+
+if(arr[i]>max1)
+{
+max2=max1;
+max1=arr[i];
+
+
+}
+
+else if(arr[i]>max2)
+{
+max2=arr[i];
+}
+}
+
+System.out.println(max1);
+System.out.println(max2);
+
+}
+
+}
+
+//15
+//12
+
+
 
