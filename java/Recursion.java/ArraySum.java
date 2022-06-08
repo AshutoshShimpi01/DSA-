d71a54f7ce41425f87874ea27eca876343384dad
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-06-08 19:22:49 +0530

    Update ArraySum.java

--- java/Recursion.java/ArraySum.java
+++ java/Recursion.java/ArraySum.java
@@ -35,3 +35,40 @@ public class reverse{
 }
 
 //21
+
+
+-----------------------------------------
+//USING SCANNER CLASS
+	
+//CODE COMPLE BUT THROW EXCEPTION ERROR
+	
+import java.util.Scanner;
+
+    public class check
+    {
+        public static int sum(int n,int arr[])
+        {
+            if(n<0) return 1;
+           return sum(n-1, arr) + arr[n];
+        }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        check ch = new check();
+        
+        int size = sc.nextInt();
+        int Arr[] = new int[size];
+        
+        for(int i=0;i<=Arr.length;i++)
+        {
+            Arr[i]=sc.nextInt();
+        }
+        
+        ch.sum(size,Arr);
+        
+       
+        System.out.println(sum(size,Arr));
+    }
+    
+}
