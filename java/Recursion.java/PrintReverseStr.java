00d80ad65ab5ec5162f7b5e07daf4f87fc09a7f9
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-06-08 13:13:01 +0530

    Update PrintReverseStr.java

--- java/Recursion.java/PrintReverseStr.java
+++ java/Recursion.java/PrintReverseStr.java
@@ -1,3 +1,38 @@
+//WRITEN BY ME
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class reverse{
+    public void func(int n)
+{
+    if(n==0)     return;
+    System.out.print(n + " ");
+    func(n-1);
+    
+ 
+}
+}
+class HelloWorld {
+    public static void main(String[] args) {
+     
+    reverse r=new reverse();
+    
+    r.func(5);
+    
+ 
+
+    }
+}
+
+//5 4 3 2 1 
+
+--------------------------------------
+
+
+
+
 public class Recursion
 {
 	public static void printRev(String str,int idx)
