993a578865d8bfb93050f1c3879a2290ccbc9be5
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-06-09 12:07:00 +0530

    Update findMax.java

--- java/Arrays.java/findMax.java
+++ java/Arrays.java/findMax.java
@@ -84,4 +84,48 @@ System.out.println(max2);
 //12
 
 
+---------------------------------------
+  
+ //FIND 2nd LARGEST WUTH FUNC/METHOD
+  
+  
+import java.util.*;
+class java
+{
+    public static int isPrime(int ar[],int max2)
+{
+    int max1 = ar[0];
+    for(int i=0;i<ar.length;i++)
+    {
+    if(ar[i]>max1)
+    {
+        max2=max1;
+        max1=ar[i];
+    }
+    else if(ar[i]>max2)
+    max2=ar[i];
+    
+}
+return max2;
+}
+    
+    public static void main(String args[])
+    {
+        
+        int arr[] = {1,2,3,4,5,6};
+        int Max4=0;
+        
+        int check = isPrime(arr,Max4);
+        
+        
+          System.out.println(check);  
+       
+        
+    }
+
+}
+
+//5
+
+
 
