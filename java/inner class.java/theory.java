74e2dcd13f200e837e6d3ebd0c43c028437e1f5a
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-18 09:05:59 +0530

    Create theory.java

---
+++ java/inner class.java/theory.java
@@ -0,0 +1,6 @@
+/*
+Sometimes users need to program a class in such a way so that no other class can access it. Therefore,
+it would be better if you include it within other classes.
+
+If all the class objects are a part of the outer object then it is easier to nest that class inside the outer class.
+That way all the outer class can access all the objects of the inner class.
