8b62aed0282c234898a19900e015ecad99d07d0b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-19 20:16:13 +0530

    Update BubbleSort.java

--- java/Sorting.java/BubbleSort.java
+++ java/Sorting.java/BubbleSort.java
@@ -1,3 +1,64 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        
+        int count = 1;
+        
+      while(count<n)
+      {
+          for(int i=0;i<n-count;i++)
+          {
+              if(arr[i]>arr[i+1])
+              {
+                  int temp = arr[i];
+                  arr[i] = arr[i+1];
+                  arr[i+1] = temp;
+              }
+          }
+          count++;
+      }
+        
+   for(int i=0;i<arr.length;i++)
+   {
+    System.out.println(arr[i]);
+   }
+        
+        
+    }
+}
+
+/*
+6
+8 4 9 2 5 3
+2
+3
+4
+5
+8
+9
+
+
+
+
+
+__________________________________________________________________
+
+
+
+
 
 // In Bubble sort we can push the largest element at last position.
 
@@ -69,3 +130,9 @@ HOW:-
 
 
 */
+
+
+__________________________________________________________________
+
+
+
