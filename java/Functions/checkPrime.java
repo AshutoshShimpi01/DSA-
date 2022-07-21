54a9a6a897b27fd823ed9effcc9e6e701c74e1b8
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-21 11:13:37 +0530

    Update checkPrime.java

--- java/Functions/checkPrime.java
+++ java/Functions/checkPrime.java
@@ -1,43 +1,35 @@
-import java.lang.*;
 import java.util.Scanner;
-
-
-
-class java
-{
-    public static int isPrime(int num)
-{
-    int reminder =0;
-    for(int i=2;i<num/2;i++)
-    {
-        reminder=i%num;
-        if(reminder==0)
-        {
-            return 1;
-        }
-        
-    }
-    return 0;
-}
+class java {
     
-    public static void main(String args[])
+    public static int isPrime(int n)
     {
+        int rem=0;
+        for(int i=2;i<n/2;i++)
+        {
+            rem = n%i;
+            if(rem==0)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
+        
+        java j = new java();
+        
         int n = sc.nextInt();
         int check = isPrime(n);
         
-        if(check == 0)
-        {
-            System.out.println("not prime");
-        }
-        else
-        {
-          System.out.println("prime");  
-        }
-        
+       if(check==1)
+       {
+        System.out.println("not prime"); 
+       }
+       if(check==0)
+       {
+        System.out.println("prime"); 
+       }
     }
-
 }
-/*
-11
-prime
