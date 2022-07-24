29b215dc2cdbb3bec5c0a4019e3361f0eebc4f7b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-24 18:39:13 +0530

    Create how StackFramework run.java

---
+++ java/basic codes/how StackFramework run.java
@@ -0,0 +1,17 @@
+class Main
+{
+   public static void run()
+    {
+        System.out.println("Bhaag");
+    }
+    public static void main(String[] args){
+
+        System.out.println("start main");
+        run();
+        System.out.println("End main");
+
+    }
+}
+
+//Bhaag
+//End main
