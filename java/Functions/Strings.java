171c8081f855ea6337a1159e00031c6458a7467c
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-27 16:22:47 +0530

    Create Strings.java

---
+++ java/Functions/Strings.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String s = "I am Ashutosh";
+        String [] str = s.split(" ");
+        
+         System.out.println(str[2]);
+       
+    }
+}
