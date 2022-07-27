f05007638862d6652405b0cf2952ae0985461198
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-27 19:43:46 +0530

    Create ReverseStrings.java

---
+++ java/Functions/ReverseStrings.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+    
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+   String s = "Vinod";
+   String str = "";
+   
+   for(int i=s.length()-1;i>=0;i--)
+   {
+       str+=String.valueOf(s.charAt(i));
+   }
+   
+   System.out.println(str);
+        
+    }
+}
+
+//doniV
