6267e119fe1c10224c3d5662d340e513ea78807f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-27 20:27:59 +0530

    Update BubbleSort.java

--- java/Sorting.java/BubbleSort.java
+++ java/Sorting.java/BubbleSort.java
@@ -136,3 +136,55 @@ __________________________________________________________________
 
 
 
+
+
+
+
+//ESAY
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+    
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    
+    int n = 6;
+    
+    int arr[] = {8,4,5,2,6,2};
+    
+    int count = 1;
+    
+    while(count < n)
+    {
+        for(int i=0;i<n-count;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        count ++;
+    }
+     for(int i=0;i<arr.length;i++)
+   {
+    System.out.println(arr[i]);
+   }
+        
+    }
+}
+
+
+
+
