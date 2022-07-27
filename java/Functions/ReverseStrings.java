738b61cd8be59f0f31a5ab0e65ace18555e4a8ca
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-27 19:47:55 +0530

    Update ReverseStrings.java

--- java/Functions/ReverseStrings.java
+++ java/Functions/ReverseStrings.java
@@ -25,3 +25,42 @@ class java
 }
 
 //doniV
+
+
+
+
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+    
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+   String s = "Vinod";
+   StringBuilder sb = new StringBuilder(s);
+   
+   System.out.println(sb.reverse());
+   //String str = "";
+   
+   /*
+   for(int i=s.length()-1;i>=0;i--)
+   {
+       str+=String.valueOf(s.charAt(i));
+   }
+   
+   System.out.println(str);
+        
+    }
+    */
+    }
+}
+
