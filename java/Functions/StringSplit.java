d4c5f5fdafe0efd941348361caad43a898dfc830
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-27 18:54:15 +0530

    Update StringSplit.java

--- java/Functions/StringSplit.java
+++ java/Functions/StringSplit.java
@@ -11,3 +11,31 @@ class HelloWorld {
        
     }
 }
+
+
+
+
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class exam
+{
+    
+
+    public static void main(String[] args) {
+    
+    String s = "i am Ashutosh";
+    
+    String str[] = s.split(" ");   //3
+    // String str[] = s.split("");  // 13
+    //System.out.println(s.length()); //13
+    //System.out.println(str[2]);  //Ashutosh
+    System.out.println(str.length);  // 3
+    
+        
+    }
+}
