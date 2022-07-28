add1cf2593668ca24b2cda599ff6b295f08efcae
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-28 17:49:52 +0530

    Update SelectionSort.java

--- java/Sorting.java/SelectionSort.java
+++ java/Sorting.java/SelectionSort.java
@@ -60,3 +60,89 @@ HOW:-
 1 2 3 7 8 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+    
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+   int n = sc.nextInt();
+   
+   int arr[] = new int[n];
+   
+   for(int i=0;i<arr.length;i++)
+   {
+       arr[i] = sc.nextInt();
+         
+   }
+   
+   for(int i=0;i<arr.length-1;i++)
+    {
+      int small = i;
+      for(int j=i+1;j<arr.length;j++)
+      {
+        if(arr[small] > arr[j])
+        {
+          small = j;
+        }
+      }
+     int temp = arr[small];
+     arr[small] = arr[i];
+     arr[i] = temp;
+     
+    }
+
+  
+   for(int i=0;i<arr.length;i++)
+   {
+       System.out.println(arr[i]);
+   }
+   
+ 
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
