7504f6fe2fdf07d27630ce4c7d2ed37869b34888
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-28 18:15:10 +0530

    Update LinearSrch.java

--- java/SeachingAlgo.java/LinearSrch.java
+++ java/SeachingAlgo.java/LinearSrch.java
@@ -1,1 +1,51 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+    
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+   int n = sc.nextInt();
+   
+   int arr[] = new int[n];
+   
+   for(int i=0;i<arr.length;i++)
+   {
+       arr[i] = sc.nextInt();
+   }
+   
+   int key = sc.nextInt();
+   
+   for(int i=0;i<arr.length;i++)
+   {
+       if(key == arr[i])
+       {
+           System.out.println(i);
+       }
+       else if(key != arr[i])
+       {
+           System.out.println(" ");
+       }
+   }
+  
+ 
+}
+}
+
+
+/*
+5
+6 5 4 3 2
+3
+3
+
+
+
+
