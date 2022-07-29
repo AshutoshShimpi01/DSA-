95973af577672ec20fb89d9708e4dcd759e50569
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-29 14:50:58 +0530

    Update patterns.java

--- java/patterns.java
+++ java/patterns.java
@@ -507,3 +507,53 @@ class java
     *
 
 */
+
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+class java
+{
+        
+  
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+  int n = sc.nextInt();
+  
+  for(int i=1;i<=n;i++)
+  {
+      for(int j=1;j<=i;j++)
+      { 
+          System.out.print(j);
+      }
+      System.out.println();
+  }
+  for(int i=n-1;i>0;i--)
+  {
+    for(int j=1;j<=i;j++)
+    {
+        System.out.print(j);
+    }
+    System.out.println();
+  }
+  
+    }
+}
+
+
+// 
+3
+1
+12
+123
+12
+1
+//
+
+
