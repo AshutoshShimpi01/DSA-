9067d84ebae690f8c403488f209d902cc48240fa
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-30 09:52:30 +0530

    Create splitsString.java

---
+++ java/InterviewAskQuestion.java/splitsString.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String s = "i am ashutosh aa";
+        
+        String str[] = s.split(" ");
+        
+        System.out.println(str[2]);
+        
+    }
+}
+
+//ashutosh
