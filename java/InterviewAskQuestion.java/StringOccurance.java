c8e6e0a501c5d06c8575aac0c275a1e1e69f6b30
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-30 12:05:49 +0530

    Update StringOccurance.java

--- java/InterviewAskQuestion.java/StringOccurance.java
+++ java/InterviewAskQuestion.java/StringOccurance.java
@@ -21,3 +21,29 @@ class HelloWorld {
 }
 
 //4
+
+----------
+    
+    
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+class HelloWorld {
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+       String s = "ashutosh Shimpi ss";
+       System.out.print(s.length());
+           
+        
+    }
+}
+
+/*
+18
+
+*/
