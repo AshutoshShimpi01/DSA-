b392646e1c029e5896b8f5e0c7b2bf81fb55150a
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-07-30 09:50:50 +0530

    Create StringOccurance.java

---
+++ java/InterviewAskQuestion.java/StringOccurance.java
@@ -0,0 +1,23 @@
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String s = " i am ashutosh aa";
+        
+        int count=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a')
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+        
+    }
+}
+
+//4
