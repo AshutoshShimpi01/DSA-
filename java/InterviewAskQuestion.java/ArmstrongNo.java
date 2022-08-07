1a0e6cc4ed79a75bac8da7f9e14b8a2249dfcfef
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-07 08:16:10 +0530

    Create ArmstrongNo.java

---
+++ java/InterviewAskQuestion.java/ArmstrongNo.java
@@ -0,0 +1,39 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+class java {
+    public static void main(String[] args) {
+       
+      Scanner sc = new Scanner(System.in);
+      
+      int n = sc.nextInt();
+      
+      int temp = n;
+      
+      int rem,res=0;
+      
+      while(temp!=0)
+      {
+          rem = temp%10;
+          res += Math.pow(rem,3);
+          temp = temp/10;
+      }
+      
+      if(res == n)
+      {
+         System.out.println("Armstrong no."); 
+      }
+     else
+      {
+         System.out.println("not Armstrong no."); 
+      }
+      
+      //153
+      //Armstrong no.
+      
+      
+      
+       
+    }
+}
