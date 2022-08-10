99fa42f68da97cda5dd7b0610f2ae4e89d31b811
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-10 17:47:36 +0530

    Create add Frst-last.java

---
+++ java/DS.java/LinkedLst.java/add Frst-last.java
@@ -0,0 +1,93 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class LL {
+   Node head;
+   class Node
+   {
+       String data;
+       Node next;
+       
+       Node(String data)
+       {
+           this.data=data;
+           this.next=null;
+       }
+       
+   }
+       // add First
+       
+       public void addFirst(String data)
+       {
+           Node newNode = new Node(data);
+           
+           if(head == null)
+           {
+              head = newNode;
+              return;
+           }
+          newNode.next=head;
+          head=newNode;
+       }
+       
+       // add Last
+       
+       public void addLast(String data)
+       {
+           Node newNode = new Node(data);
+           
+           if(head == null)
+           {
+              head = newNode;
+              return;
+           }
+           
+           Node currentNode=head;
+           while(currentNode.next!=null)
+           {
+               currentNode = currentNode.next;
+           }
+           currentNode.next=newNode;
+       }
+       
+       public void printList()
+       {
+           if(head == null)
+           {
+               System.out.println("List is empty");
+               return;
+           }
+           Node currentNode=head;
+           while(currentNode!=null)
+           {
+               System.out.print(currentNode.data + " -> ");
+               currentNode = currentNode.next;
+           }
+           System.out.print("Null");
+       
+       }
+       
+       
+   
+    
+    public static void main(String[] args) {
+        
+        LL list = new LL();
+        
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        
+        list.addLast("list");
+        list.printList();
+        
+        list.addFirst("list");
+        list.printList();
+    }
+}
+
+/*
+is -> a -> Null
+is -> a -> list -> Nulllist -> 
+is -> a -> list -> Null
+*/
