9ba021700e4b4f80fd97e6124c087fcfe8dda194
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-15 20:31:58 +0530

    Create basic.java

---
+++ java/DS.java/ArrayList.java/basic.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.*; 
+class test
+{
+    public static void main(String[] args) {
+        
+       
+      List<String> li=new ArrayList<>();
+       
+       li.add("ashu");
+       li.add("keshu");
+       li.add("babu");
+       li.add("gotu");
+       
+       for (String str : li)
+       {  
+      System.out.println(str);  
+       }  
+       
+    }
+}
+
+/*
+ashu
+keshu
+babu
+gotu
+*/
