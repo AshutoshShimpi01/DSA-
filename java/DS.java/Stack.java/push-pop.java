43f0826a70173c0b4c521e0ec450ce1d2884c102
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-17 16:45:34 +0530

    Update push-pop.java

--- java/DS.java/Stack.java/push-pop.java
+++ java/DS.java/Stack.java/push-pop.java
@@ -1,1 +1,76 @@
+public class java {
 
+
+    static Node head;
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static boolean isEmpty() {
+        return head == null;
+    }
+
+    public static void add(int data) {
+
+        Node newNode = new Node(data);
+
+
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public static int pop() {
+
+        if(isEmpty()) {
+            return -1;
+        }
+
+        int temp = head.data;
+        head = head.next;
+        return temp;
+    }
+
+    public static int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return head.data;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        add(13);
+        add(81);
+        add(12);
+        add(99);
+        add(43);
+
+        System.out.println(peek());
+
+        while(!isEmpty()) {
+            System.out.println(pop());
+        }
+    }
+}
+
+
+// 43
+// 43
+// 99
+// 12
+// 81
+// 13
