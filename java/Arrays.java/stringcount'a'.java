fc302b3c3c4d88df15369b1b28910a51a4e0d2d0
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-18 11:01:44 +0530

    Update stringcount'a'.java

--- java/Arrays.java/stringcount'a'.java
+++ java/Arrays.java/stringcount'a'.java
@@ -54,3 +54,37 @@ class java {
 // asakjh
 // ajhga
 // aaj
+
+-----------------------
+    
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class java {
+    public static void main(String[] args) {
+      String s = "i am ashu aat akait aakau jay. asakjh ajhga sman aaj";
+
+
+      String arr[] = s.split(" ");
+
+      for(int i=0;i<arr.length;i++)
+      {
+
+      if(arr[i].endsWith("a"))
+
+
+      {
+          System.out.println(arr[i]);
+        }
+      }
+    }
+}
+
+// am
+// ashu
+// aat
+// akait
+// aakau
+// asakjh
+// ajhga
+// aaj
