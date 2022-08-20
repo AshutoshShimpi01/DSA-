072bc0f6c002886b7d0ecd392eb7b7cef938142c
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-20 18:57:29 +0530

    Update checkFrequencyOfarray.java

--- java/InterviewAskQuestion.java/checkFrequencyOfarray.java
+++ java/InterviewAskQuestion.java/checkFrequencyOfarray.java
@@ -66,3 +66,62 @@ class java {
 // ---------------
 //  7 1
 // ---------------
+
+
+
+
+//WRONG
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+       
+        int arr[] = {1,2,3,2,1,3,4};
+        
+        int fr[] = new int[arr.length];
+        
+        int check = -1;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            int count = 1;
+            
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j]);
+                {
+                    count++;
+                    fr[j]=check;
+                }
+               
+            }
+             if(fr[i]!=check)
+                {
+                    fr[i]=count;
+                }
+            
+            }
+            
+           System.out.println("---------------");  
+        System.out.println(" Element | Frequency");  
+        System.out.println("---------------"); 
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(fr[i]!=check)
+            {
+                System.out.println(" "+ arr[i] + " " + fr[i]);
+            }
+            System.out.println("---------------");   
+        }
+        
+    }
+}
