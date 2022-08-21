354f8496fc3c012a7ac6a638d58920b99ed5dc47
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-21 20:48:47 +0530

    Create reverseArr.java

---
+++ java/Arrays.java/reverseArr.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        
+       int [] arr = new int [] {1, 2, 3, 4, 5};  
+      
+      
+      for(int i=arr.length-1;i>=0;i--)
+    {
+        System.out.print(arr[i] + " ");
+    }
+    
+    }
+}
+
+//5 4 3 2 1
