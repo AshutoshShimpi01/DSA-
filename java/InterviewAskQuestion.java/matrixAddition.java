ca79ddcf90870f6adbfaece75dd7273ad2eb6aef
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-21 11:22:50 +0530

    Update matrixAddition.java

--- java/InterviewAskQuestion.java/matrixAddition.java
+++ java/InterviewAskQuestion.java/matrixAddition.java
@@ -29,3 +29,58 @@ class HelloWorld {
 // 2 4 6 
 // 8 10 12 
 // 14 16 18 
+
+
+
+
+// input from user 
+
+
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    
+    int a[][] = new int[n][m];
+    int b[][] = new int[n][m];
+    
+    
+   for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < m; j++) 
+                {
+                   a[i][j] = sc.nextInt();
+                   b[i][j] = sc.nextInt();
+                }
+                System.out.println("");
+            }
+    
+    
+    int c[][] = new int[n][n];
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+           c[i][j]=a[i][j]+b[i][j]; 
+           System.out.print(c[i][j] + " ");
+        }
+        System.out.println();
+    }
+    
+    
+    
+    }
+}
+
+
+// 3 3
+// 1 2 3 4 5 6 7 8 9
+// 3 3
+// 1 2 1 2 3 4 5 5 6 
+
+// 5 9 13 
+// 17 6 3 
+// 3 7 10 
