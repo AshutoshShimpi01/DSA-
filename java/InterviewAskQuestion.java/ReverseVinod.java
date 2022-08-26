1a8b2f0ba8f06e5eb94fcb3d8c96fb7401539291
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-26 16:32:39 +0530

    Update ReverseVinod.java

--- java/InterviewAskQuestion.java/ReverseVinod.java
+++ java/InterviewAskQuestion.java/ReverseVinod.java
@@ -17,3 +17,50 @@ class HelloWorld {
 }
 
 //doniv
+
+
+
+
+---------------
+    
+ 
+    
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String s = "nayan";
+        
+        String str = "";
+        
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            str+=String.valueOf(s.charAt(i));
+        }
+        System.out.println(str);
+        System.out.println(" ");
+        
+      System.out.println(s.compareTo(str));
+      
+   
+    }
+}
+
+// nayan
+// 0
+
+// doniv
+// 18
+
+
+
+
+ //     if(s==str)
+    //     {
+    //         System.out.println(" palindrome ");
+    //     }
+    //   else
+    //     {
+    //         System.out.println(" not palindrome ");
+    //     }
