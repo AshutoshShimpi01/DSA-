285a7f85179f3bbf9bd2d92b6055b1c90a9e4a99
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-08-26 17:20:03 +0530

    Update ReverseVinod.java

--- java/InterviewAskQuestion.java/ReverseVinod.java
+++ java/InterviewAskQuestion.java/ReverseVinod.java
@@ -64,3 +64,102 @@ class HelloWorld {
     //     {
     //         System.out.println(" not palindrome ");
     //     }
+
+
+
+
+--------------
+    
+    
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class java {
+    public static void main(String[] args) {
+      String s = "i am ashu aat akait aakau jay. asakjh ajhga sman aaj";
+
+
+      String[] arr = s.split(" ");
+
+      for(int i = 0; i < arr.length; i++) {
+        if(arr[i].charAt(0) == 'a') {
+          System.out.println(arr[i]);
+        }
+      }
+    }
+}
+
+// am
+// ashu
+// aat
+// akait
+// aakau
+// asakjh
+// ajhga
+// aaj
+
+
+--------------------------
+    
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class java {
+    public static void main(String[] args) {
+      String s = "i am ashu aat akait aakau jay. asakjh ajhga sman aaj";
+
+
+      String[] arr = s.split(" ");
+
+      for(int i = 0; i < arr.length; i++) {
+        if(arr[i].startsWith("a")) {
+          System.out.println(arr[i]);
+        }
+      }
+    }
+}
+
+// am
+// ashu
+// aat
+// akait
+// aakau
+// asakjh
+// ajhga
+// aaj
+
+-----------------------
+    
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class java {
+    public static void main(String[] args) {
+      String s = "i am ashu aat akait aakau jay. asakjh ajhga sman aaj";
+
+
+      String arr[] = s.split(" ");
+
+      for(int i=0;i<arr.length;i++)
+      {
+
+      if(arr[i].endsWith("a"))
+
+
+      {
+          System.out.println(arr[i]);
+        }
+      }
+    }
+}
+
+// am
+// ashu
+// aat
+// akait
+// aakau
+// asakjh
+// ajhga
+// aaj
+
+
