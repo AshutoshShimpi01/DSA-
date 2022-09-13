b07991e69a004687883444df87817307769492ad
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-13 17:55:57 +0530

    Update patterns.java

--- java/patterns.java
+++ java/patterns.java
@@ -1,4 +1,36 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+      int n = 1;
+      
+      
+      for(int i=1;i<=3;i++)
+      {
+          for(int j=1;j<=3;j++)
+          {
+              System.out.print((n*n + " "));
+              n++;
+          }
+          System.out.println();
+      }
+    }
+}
+
+// 1 4 9 
+// 16 25 36 
+// 49 64 81 
+
+
+
+
+
+----------------
+	
+	
 public class ja
 {
  public static void main(String[] args) 
