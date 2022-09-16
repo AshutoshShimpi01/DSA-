78ef0639cfebb686ff7c466ab15e74f3693376ed
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-16 21:12:57 +0530

    Create Ststic keyword.java

---
+++ java/basic codes/Ststic keyword.java
@@ -0,0 +1,7 @@
+// No, we cannot override static methods because method overriding is based on dynamic binding at runtime and
+// the static methods are bonded using static binding at compile time.
+  
+  
+  
+//   Can we overload static methods? The answer is 'Yes'.
+//   We can have two or more static methods with the same name, but differences in input parameters.
