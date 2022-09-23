a08f5107e9b6d0e0b053ca1e35bdcf80ffa552f6
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-23 19:38:36 +0530

    Create Copy-Constructor.java

---
+++ java/basic codes/Copy-Constructor.java
@@ -0,0 +1,39 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class A {
+    
+    int x,y;
+    
+    A()
+    {
+        x=10;
+        y=11;
+        
+        System.out.println(x + " " + y);
+    }
+    
+    A(A ref)
+    {
+        x=ref.x;
+        y=ref.y;
+        
+        System.out.println(x + " " + y);
+    }
+    
+}
+
+class C
+{
+    
+    public static void main(String[] args) {
+        
+        A r = new A();
+        A r2 = new A(r);
+        
+        
+    }
+}
+
+// 10 11
+// 10 11
