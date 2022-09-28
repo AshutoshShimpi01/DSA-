75c159983e25b89f55bd89d8fa8b5da66257b2cb
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-28 19:41:21 +0530

    Update removeDublicate-Array.java

--- java/InterviewAskQuestion.java/removeDublicate-Array.java
+++ java/InterviewAskQuestion.java/removeDublicate-Array.java
@@ -1,3 +1,65 @@
+import java.util.Scanner;
+
+class java
+{
+    // Function to remove duplicate elements
+    // This function returns new size of modified
+    // array.
+    static int removeDuplicates(int arr[], int n)
+    {
+        if (n == 0 || n == 1)
+            return n;
+      
+        // To store index of next unique element
+        int j = 0;
+      
+        // Doing same as done in Method 1
+        // Just maintaining another updated index i.e. j
+        for (int i = 0; i < n-1; i++)
+            if (arr[i] != arr[i+1])
+                arr[j++] = arr[i];
+      
+        arr[j++] = arr[n-1];
+      
+        return j;
+    }
+     
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        n = removeDuplicates(arr, n);
+  
+        // Print updated array
+        for (int i=0; i<n; i++)
+           System.out.print(arr[i]+" ");
+    }
+}
+5   
+1 1 2 3 3
+1 2 3 %                                                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
