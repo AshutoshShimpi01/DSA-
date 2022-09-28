2f4a3a0811a85627e73bd36d125318711d86b37d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-28 15:34:36 +0530

    Update splitsString.java

--- java/InterviewAskQuestion.java/splitsString.java
+++ java/InterviewAskQuestion.java/splitsString.java
@@ -14,3 +14,25 @@ class HelloWorld {
 }
 
 //ashutosh
+
+
+----------------
+    
+    
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String s = "vinod";
+        
+        String str[] = s.split("");
+        
+        System.out.print(str[4]);
+        
+        
+    }
+}
+
+//d
