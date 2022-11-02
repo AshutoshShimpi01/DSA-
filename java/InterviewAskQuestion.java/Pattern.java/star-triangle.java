f5d55e442a24435b10107e9351683d2a3d432d1b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-11-02 20:34:06 +0530

    Create star-triangle.java

---
+++ java/InterviewAskQuestion.java/Pattern.java/star-triangle.java
@@ -0,0 +1,35 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+import java.util.Scanner;
+class code {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                    System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                    System.out.print(" *"); // space imp
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+// 5
+//       *
+//      * *
+//     * * *
+//    * * * *
+//   * * * * *
