b06dce9aba5bc3ce4046cd0acfe8757ba46b0073
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-11-10 20:06:44 +0530

    Update call-By-value.java

--- java/basic codes/call-By-value.java
+++ java/basic codes/call-By-value.java
@@ -2,10 +2,18 @@
 // If we pass object in place of any primitive value, original value will be changed.
 // In this example we are passing object as a value. Let's take a simple example:
 
-
 //temporary value changes inside functions not parmantly changes.
 //temporary changes perform inside functions, not parmantly change the original value of primitive datatype.
 
+
+
+
+
+// There is only call by value in java, not call by reference.If we call a method passing a value, it is known as call by value.
+// The changes being done in the called method, is not affected in the calling method.
+    
+// In case of call by reference original value is changed if we made changes in the called method.
+// If we pass object in place of any primitive value, original value will be changed. 
 
 
 class HelloWorld {
