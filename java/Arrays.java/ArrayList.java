b231e5c34625258bc75b32e8b3b6b8ee50fe0fea
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2024-09-14 17:48:01 +0530

    Create ArrayList.java

---
+++ java/Arrays.java/ArrayList.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+import java.util.*;  
+public class ArrayListExample2{  
+ public static void main(String args[]){  
+
+  List<String> list1 = new ArrayList<String>();
+
+  list1.add("mango");
+  list1.add("tomato");
+  list1.add("chiku"); 
+  
+
+   Collections.sort(list1);  
+
+  
+
+for(String fruit:list1)  
+    System.out.println(fruit);  
+
+ }  
+}  
